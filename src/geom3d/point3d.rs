5d8b8babcd383a42c2be93cd96eb98e819f7e0d7
mauroalberti <alberti.m65@gmail.com>
2021-03-31 22:24:33 +0200

    Implemented basic methods

--- src/geom3d/point3d.rs
+++ src/geom3d/point3d.rs
@@ -1,7 +1,6 @@
 use pyo3::prelude::*;
 
 #[pyclass]
-#[derive(Default)]
 pub struct Point3D {
     pub x: f64,
     pub y: f64,
@@ -12,24 +11,27 @@ pub struct Point3D {
 impl Point3D {
 
     #[new]
-    fn new() -> Self {
-        Point3D::default()
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Point3D { x, y, z}
     }
 
-    pub fn delta_x(&self, other: &Self) -> f64 {
-        other.x - self.x
+
+    pub fn delta_x(&self, other: &Self) -> PyResult<f64> {
+        Ok(other.x - self.x)
     }
 
-    pub fn delta_y(&self, other: &Self) -> f64 {
-        other.y - self.y
+    pub fn delta_y(&self, other: &Self) -> PyResult<f64> {
+        Ok(other.y - self.y)
     }
 
-    pub fn delta_z(&self, other: &Self) -> f64 {
-        other.z - self.z
+    pub fn delta_z(&self, other: &Self) -> PyResult<f64> {
+        Ok(other.z - self.z)
     }
 
+    /*
     pub fn dist3d(&self, other: &Self) -> f64 {
         (self.delta_x(other) * self.delta_x(other) + self.delta_y(other) * self.delta_y(other) + self.delta_z(other) * self.delta_z(other)).sqrt()
     }
+     */
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 use pyo3::prelude::*;
-use pyo3::{wrap_pyfunction, wrap_pymodule};
-use pyo3::types::IntoPyDict;
+use pyo3::{Py, PyResult, Python};
 
 use crate::geom2d::Point2D;
 use crate::geom2d::Segment2D;
@@ -12,47 +11,15 @@ mod geom2d;
 mod geom3d;
 
 
-#[pyfunction]
-fn dist2d(x1: f64, y1: f64, x2: f64, y2: f64) -> PyResult<f64> {
-
-    let p1 = Point2D{x: x1, y: y1};
-    let p2 = Point2D{x: x2, y: y2};
-
-    let out = p1.dist2d(&p2);
-    Ok(out)
-
-}
-
-fn init_submod_geom2d(module: &PyModule) -> PyResult<()> {
-    module.add_function(wrap_pyfunction!(dist2d, module)?)?;
-    Ok(())
-}
-
-#[pyfunction]
-fn dist3d(x1: f64, y1: f64, z1: f64, x2: f64, y2: f64, z2: f64) -> PyResult<f64> {
-
-    let p1 = Point3D{x: x1, y: y1, z: z1};
-    let p2 = Point3D{x: x2, y: y2, z: z2};
-
-    let out = p1.dist3d(&p2);
-    Ok(out)
-
-}
-
-fn init_submod_geom3d(module: &PyModule) -> PyResult<()> {
-    module.add_function(wrap_pyfunction!(dist3d, module)?)?;
-    Ok(())
-}
-
 #[pymodule]
 fn misah(py: Python, m: &PyModule) -> PyResult<()> {
 
     let geom2d = PyModule::new(py, "geom2d")?;
-    init_submod_geom2d(geom2d)?;
+
     m.add_submodule(geom2d)?;
 
     let geom3d = PyModule::new(py, "geom3d")?;
-    init_submod_geom3d(geom3d)?;
+
     m.add_submodule(geom3d)?;
 
     m.add_class::<Point3D>()?;
